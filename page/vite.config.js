80eb7e4ce6f092810d1aa0d562fdef3f82f6de52
LuisFOsG <luisfosgdev@gmail.com>
2022-04-12 17:32:22 -0500

    chore(Root): build and deploy page

--- page/vite.config.js
+++ page/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(), createHtmlPlugin({
+      minify: true
+    })
+  ]
 })
