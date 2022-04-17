effd452b61c088197a972751c10ddfb68c271446
LuisFOsG <luisfosgdev@gmail.com>
2022-04-16 21:09:55 -0500

    feat(Root): download code zip and add code syntax highlighter

--- page/src/components/App/index.jsx
+++ page/src/components/App/index.jsx
@@ -1,14 +1,35 @@
+import Prism from 'prismjs'
+import 'prismjs/themes/prism.css'
+
 import styles from './app.module.css'
 
 import Header from '../Header'
 
+const CODE = `
+<iframe width='100%' height='100%' src='https://map-unipaz.surge.sh/map'></iframe>
+`
+
+const HTML = Prism.highlight(CODE, Prism.languages.html, 'html')
+
 const App = () => {
+  console.log(HTML)
+
   return (
     <>
       <Header />
       <div className={styles.app}>
         <a href='/map'>Visita el mapa aqui</a>
-        <iframe src='/map'/>
+        <iframe src='/map' /><br />
+        <div>
+          <h2>El codigo fuente del mapa en modo producción</h2>
+          <a href='/map.zip' download>
+            Descarga el codigo fuente aqui
+          </a>
+
+          <br /><br />
+          <h2>Coloca el mapa en tu pagina web</h2>
+          <div className={styles.code} dangerouslySetInnerHTML={{ __html: HTML }} />
+        </div>
       </div>
     </>
   )
