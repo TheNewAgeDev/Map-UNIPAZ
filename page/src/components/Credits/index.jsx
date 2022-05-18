6cd04e94f97e4eb9d7cd50960e6dae6d5d910f05
LuisFOsG <luisfosgdev@gmail.com>
2022-05-18 16:02:07 -0500

    refactor(page): custom styles css page

--- page/src/components/Credits/index.jsx
+++ page/src/components/Credits/index.jsx
@@ -1,7 +1,41 @@
+import ReactTypingEffect from 'react-typing-effect'
+
+import styles from './credits.module.css'
+
+const CONTRIBUTORS = [
+  'Natalia Rodríguez',
+  'Luis Osorio',
+  'Jhan Lopez',
+  'Breinner Rueda',
+  'Javier Rodríguez',
+  'Bryan Muñoz',
+  'Aldair Luque'
+]
+
+const Typing = () => {
+  return (
+    <ReactTypingEffect
+      text={CONTRIBUTORS}
+      speed={100}
+      eraseSpeed={50}
+      eraseDelay={500}
+      typingDelay={100}
+      cursorRenderer={cursor => <h2 className={styles['credits-color']}>{cursor}</h2>}
+      displayTextRenderer={(text, i) => {
+        return (
+          <h2 className={styles['credits-color']} key={i}>
+            {text}
+          </h2>
+        )
+      }}
+    />
+  )
+}
+
 const Credits = () => {
   return (
-    <div>
-      Mapa Creado Por: ("Nombres")
+    <div className={styles.credits}>
+      Mapa Creado Por<br /> <Typing />
     </div>
   )
 }
--- page/src/components/Footer/index.jsx
+++ page/src/components/Footer/index.jsx
@@ -1,7 +1,9 @@
+import styles from './footer.module.css'
+
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className={styles.footer}>
+      UNIMAPZ - 2022
     </div>
   )
 }
